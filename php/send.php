bd10a00792c20ecb5d1aa4e5bb774cf838b05a85
AlexGidr <102025184+AlexGidr@users.noreply.github.com>
2022-05-24 20:59:33 +0300

    change info

--- php/send.php
+++ php/send.php
@@ -1,20 +1,12 @@
 <?php
-$fio = $_POST['fio'];
 $tel = $_POST['tel'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
 $tel = htmlspecialchars($tel);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
 $tel = urldecode($tel);
-$email = urldecode($email);
-$fio = trim($fio);
 $tel = trim($tel);
-$email = trim($email);
 //echo $fio;
 //echo "<br>";
 //echo $email;
-if (mail("alexpestyshev@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: https://alexgidr.github.io/mobile-repair/# \r\n"))
+if (mail("alexpestyshev@mail.ru", "Заявка с сайта", "Телефон:".$tel ,"From: info@remont-telephonov.tech \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
